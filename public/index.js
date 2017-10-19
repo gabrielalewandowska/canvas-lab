a359ca1f625df954f874fbbf0d5a5263a4edfef3
David Mulligan <fmu2174304@hotmail.com>
2017-10-19 12:12:22 +0100

    Add ability to draw dots on the canvas

--- public/index.js
+++ public/index.js
@@ -2,6 +2,26 @@ var app = function () {
   var canvas = document.querySelector("#main-canvas");
   var context = canvas.getContext("2d");
 
+  var thinDraw = function(x, y) {
+    context.beginPath();
+    context.arc(x, y, 2, 0, Math.PI * 2, true);
+    context.fill();
+}
+
+canvas.addEventListener("mouseover", function(event) {
+    if(event.buttons ===  0){
+      thinDraw(event.x, event.y);
+    }
+})
+
+
+
+
+
+
+
+
+
 }
 
 window.addEventListener("load", app);
